b3210925656120b20b0acd262aa7d9482e359b74
Vatsal <vatsal.jha@flixstock.com>
2023-09-12 23:19:26 +0530

    comments added

--- run.py
+++ run.py
@@ -11,40 +11,52 @@ from basicsr.archs.basicvsr_arch import BasicVSR
 from basicsr.data.data_util import read_img_seq
 from basicsr.utils.img_util import tensor2img
 
-
 class Bolt:
-    def __init__(self, opts, gpu_id = 0):
-        self.model_path = opts["model_path"]
-        self.input_path = opts["input_path"]
-        self.save_path = opts["save_path"]
-        self.interval = opts["interval"]
+    def __init__(self, opts, gpu_id=0):
+        # Initialize Bolt class with options and GPU ID
+        self.model_path = opts["model_path"]  # Path to the pre-trained model
+        self.input_path = opts["input_path"]  # Path to the input video
+        self.save_path = opts["save_path"]    # Path to save output frames
+        self.interval = opts["interval"]      # Frame processing interval
         self.device = torch.device(f'cuda:{gpu_id}' if torch.cuda.is_available() else 'cpu')
+        # Create an instance of the BasicVSR model
         self.model = BasicVSR(num_feat=64, num_block=30)
+        # Load the pre-trained model's weights
         self.model.load_state_dict(torch.load(self.model_path)['params'], strict=True)
+        # Set the model in evaluation mode and move it to the specified device
         self.model.eval()
         self.model.to(self.device)
+        # Create the save path directory if it doesn't exist
         os.makedirs(self.save_path, exist_ok=True)
+        # Extract frames from the input video and get its FPS
         self.img_list, self.fps = self.__vid2img(self.input_path)
+
     def __vid2img(self, input_path):
+        # Function to convert a video into a list of frames
         vidcap = cv2.VideoCapture(input_path)
-        fps = vidcap.get(cv2.CAP_PROP_FPS)
-        success,image = vidcap.read()
+        fps = vidcap.get(cv2.CAP_PROP_FPS)  # Get frames per second (FPS) of the video
+        success, image = vidcap.read()
         count = 0
         frame_list = []
         while success:
-            frame_list.append(image)
-            success,image = vidcap.read()
-
+            frame_list.append(image)  # Append each frame to the list
+            success, image = vidcap.read()
             count += 1
         return frame_list, fps
+
     def compare_images(self, target, ref):
+        # Function to compare the quality of two images
         scores = []
+        # Convert the images to PIL format
         PIL_image_gt = Image.fromarray(np.uint8(ref)).convert('RGB')
         PIL_image_output = Image.fromarray(np.uint8(target)).convert('RGB')
+        # Calculate and append PSNR and SSIM scores
         scores.append(psnr(target, ref))
         scores.append(compare_ssim(PIL_image_output, PIL_image_gt))
         return scores
+
     def inference(self):
+        # Perform super-resolution on frames and calculate quality metrics
         num_imgs = len(self.img_list)
         psnr = []
         ssim = []
@@ -52,24 +64,27 @@ class Bolt:
         print("~~~~~Generating SR FRAMES~~~~~~")
         for idx in tqdm(range(0, num_imgs, self.interval)):
             interval = min(self.interval, num_imgs - idx)
+            # Read a batch of frames for processing
             imgs = read_img_seq(self.img_list[idx:idx + interval], return_imgname=False)
             imgs_ = imgs.unsqueeze(0).to(self.device)
             with torch.no_grad():
+                # Perform super-resolution using the model
                 outputs = self.model(imgs_)
             outputs = outputs.squeeze()
             outputs = list(outputs)
             for num, img in enumerate(outputs):
                 img_num = num + idx
-                output = tensor2img(img)
-                output_frame.append(output)
+                output = tensor2img(img)  # Convert the tensor to a numpy image
+                output_frame.append(output)  # Store the super-resolved frame
                 input_img = imgs[num]
                 input_frame_numpy = self.img_list[img_num]
                 input_frame_1024 = cv2.resize(input_frame_numpy, (1024, 1024))
+                # Compare the super-resolved frame with the original and store quality metrics
                 score = self.compare_images(output, input_frame_1024)
                 psnr.append(score[0])
                 ssim.append(score[1])
                 if idx == 0 and num == 0:
-                    h_stack = np.hstack((output, input_frame_1024))
+                    h_stack = np.hstack((output, input_frame_1024))  # Create a horizontally stacked image
+                # Save the super-resolved frame
                 cv2.imwrite(os.path.join(self.save_path, f'{num}_{idx}_BasicVSR.png'), output)
-        return np.mean(psnr),np.mean(ssim), h_stack, output_frame, self.fps
-
+        return np.mean(psnr), np.mean(ssim), h_stack, output_frame, self.fps
--- unit_test.py
+++ unit_test.py
@@ -1,37 +1,53 @@
-from run import Bolt
-import cv2
-import numpy as np 
+from run import Bolt  # Import the Bolt class from the 'run' module
+import cv2  # Import OpenCV for video processing
+import numpy as np  # Import NumPy for numerical operations
 
 if __name__ == '__main__':
+    # Define options for the first Bolt object
     opts_1 = {
         "model_path":"/content/drive/MyDrive/chkpts/net_g_300000.pth",
         "input_path":"/content/BasicVSRComparison/video1_256p.mp4",
         "save_path":"/content/BasicVSRComparison/video_frame_path_3",
         "interval":7,
     }
+
+    # Define options for the second Bolt object
     opts_2 = {
         "model_path":"/content/drive/MyDrive/chkpts/net_g_400000.pth",
         "input_path":"/content/BasicVSRComparison/video1_256p.mp4",
         "save_path":"/content/BasicVSRComparison/video_frame_path_4",
         "interval":7,
     }
-    obj_3k = Bolt(opts_1)
-    obj_4k = Bolt(opts_2)
-    psnr_3k,ssim_3k, h_stack_3k, output_frame_3k, fps_3k = obj_3k.inference()
-    psnr_4k,ssim_4k, h_stack_4k, output_frame_4k, fps_4k = obj_4k.inference()
+
+    # Create two Bolt objects with different options
+    obj_3k = Bolt(opts_1)  # Instantiate the first Bolt object
+    obj_4k = Bolt(opts_2)  # Instantiate the second Bolt object
+
+    # Perform super-resolution and quality assessment for both objects
+    psnr_3k, ssim_3k, h_stack_3k, output_frame_3k, fps_3k = obj_3k.inference()
+    psnr_4k, ssim_4k, h_stack_4k, output_frame_4k, fps_4k = obj_4k.inference()
+
+    # Save horizontally stacked images to see the difference between the original frame and SR frame
     cv2.imwrite(f"/content/BasicVSRComparison/stack_3k/img_stack.jpg", h_stack_3k)
     cv2.imwrite(f"/content/BasicVSRComparison/stack_4k/img_stack.jpg", h_stack_4k)
+
+    # Define output video paths
     pathOut_3k = "/content/BasicVSRComparison/video_3k/output.avi"
     pathOut_4k = "/content/BasicVSRComparison/video_4k/output.avi"
-    out_3k = cv2.VideoWriter(pathOut_3k,cv2.VideoWriter_fourcc(*'DIVX'), fps_3k, (1024, 1024))
-    out_4k = cv2.VideoWriter(pathOut_4k,cv2.VideoWriter_fourcc(*'DIVX'), fps_4k, (1024, 1024))
+
+    # Create video writers for the output videos
+    out_3k = cv2.VideoWriter(pathOut_3k, cv2.VideoWriter_fourcc(*'DIVX'), fps_3k, (1024, 1024))
+    out_4k = cv2.VideoWriter(pathOut_4k, cv2.VideoWriter_fourcc(*'DIVX'), fps_4k, (1024, 1024))
+
+    # Write super-resolved frames to the output videos
     for i in range(len(output_frame_3k)):
-        # writing to a image array
         out_3k.write(output_frame_3k[i])
         out_4k.write(output_frame_4k[i])
+
+    # Release the video writers
     out_3k.release()
     out_4k.release()
 
-    print(f"PSNR and ssim value of 3K chckpoint are {psnr_3k} and {ssim_3k}")
-    print(f"PSNR and ssim value of 4K chckpoint are {psnr_4k} and {ssim_4k}")
-    # print(np.mean(psnr), np.mean(ssim), fps)
+    # Print PSNR and SSIM values for both checkpoints
+    print(f"PSNR and SSIM value of 3K checkpoint are {psnr_3k} and {ssim_3k}")
+    print(f"PSNR and SSIM value of 4K checkpoint are {psnr_4k} and {ssim_4k}")
